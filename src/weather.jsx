38c9dd651a930b716b105f2d2512395d7c256b25
Maiara Muller <maiara.15muller@outlook.com>
2023-11-10 21:06:09 -0300

    app clima ok

---
+++ src/weather.jsx
@@ -0,0 +1,74 @@
+import axios from "axios";
+import { useState } from "react";
+import "./weather.css";
+
+const Weather = () => {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+
+  const apiKey = "8cd68c1a09d691af2d231e48fb632f79";
+  const urlBase = "https://api.openweathermap.org/data/2.5/weather?q=";
+  const imgUrl = "https://openweathermap.org/img/wn/";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (city) {
+      try {
+        const url = `${urlBase}${city}&appid=${apiKey}&units=metric&lang=pt_br`;
+        const response = await axios.get(url);
+        setWeatherData(response.data);
+        console.log(url);
+        setCity("");
+      } catch (error) {
+        alert(error);
+      }
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <div id="input">
+            <input
+              type="text"
+              placeholder="Digite a cidade..."
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <button type="submit">Buscar</button>
+          </div>
+          <img src="/img/previsao.jpg" alt="imagem do tempo" />
+        </form>
+      </div>
+      {weatherData !== null && (
+        <div className="weather-data">
+          <h6>Tempo agora em </h6>
+          <div className="description-container">
+            <h2>
+              {weatherData.name} - {weatherData.sys.country}
+            </h2>
+            <img
+              className="weather-icon"
+              src={`${imgUrl}${weatherData.weather[0].icon}.png`}
+              width="80"
+            />
+            <p className="description">{weatherData.weather[0].description}</p>
+          </div>
+          <div className="details-container">
+            <p className="temperature">{Math.floor(weatherData.main.temp)}ºC</p>
+            <p className="informations">
+              Sensação térmica: {Math.floor(weatherData.main.feels_like)}ºC
+            </p>
+            <p className="informations">
+              Min.: {Math.floor(weatherData.main.temp_min)}ºC Max.:{" "}
+              {Math.floor(weatherData.main.temp_max)}ºC
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Weather;
